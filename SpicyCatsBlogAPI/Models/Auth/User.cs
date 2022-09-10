520fd3673adc809dc721387fb07ae9f0f8a293d2
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-09-10 12:26:38 +0600

    Update User.cs

--- SpicyCatsBlogAPI/Models/Auth/User.cs
+++ SpicyCatsBlogAPI/Models/Auth/User.cs
@@ -6,6 +6,7 @@
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string Role { get; set; }
 
         public string RefreshToken { get; set; }
         public DateTime TokenCreated { get; set; }
