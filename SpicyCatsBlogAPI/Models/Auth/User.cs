1beb30c4c63b56c34f9c4f8f4b8854a2a62f9f9a
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-09-10 09:28:10 +0600

    disable nullable

--- SpicyCatsBlogAPI/Models/Auth/User.cs
+++ SpicyCatsBlogAPI/Models/Auth/User.cs
@@ -2,7 +2,7 @@
 {
     public class User
     {
-        public string? Id { get; set; }
+        public string Id { get; set; }
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
