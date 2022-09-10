b5d09d097be68c12bb8de6dbb715efe6baae2f14
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-09-10 09:36:37 +0600

    Create AuthRepo.cs

---
+++ SpicyCatsBlogAPI/Data/Auth/AuthRepo.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpicyCatsBlogAPI.Models.Auth;
+
+namespace SpicyCatsBlogAPI.Data.Auth
+{
+    public class AuthRepo : IAuthRepo
+    {
+        private readonly AppDbContext _ctx;
+
+        public AuthRepo(AppDbContext appDbContext)
+        {
+            _ctx = appDbContext;
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            await _ctx.Users.AddAsync(user);
+        }
+
+        public async Task<User> GetUserAsync(string userName)
+        {
+            var nameMatchedUsers = await _ctx.Users.Where(user => user.Username.Equals(userName)).ToListAsync();
+            var caseMathcedUser = nameMatchedUsers.First(user => user.Username.Equals(userName));
+
+            return caseMathcedUser;
+        }
+
+        public async Task<string> GetUserId(string userName)
+        {
+            return (await this.GetUserAsync(userName)).Id;
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            if (await _ctx.SaveChangesAsync() > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
