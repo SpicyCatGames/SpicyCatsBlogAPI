ce621c0d2ae46874a1225e3c980732ff11c2bf75
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-09-15 11:51:51 +0600

    Add JWT property to UserDto.cs

--- SpicyCatsBlogAPI/Models/Auth/UserDto.cs
+++ SpicyCatsBlogAPI/Models/Auth/UserDto.cs
@@ -11,5 +11,6 @@ namespace SpicyCatsBlogAPI.Models.Auth
         public string Username { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
+        public string JWT { get; set; } = string.Empty;
     }
 }
