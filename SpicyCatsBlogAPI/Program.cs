a99c3192208a8d9e061d0720d6d3dae6d6d73900
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-09-15 11:23:21 +0600

    Swagger always on

--- SpicyCatsBlogAPI/Program.cs
+++ SpicyCatsBlogAPI/Program.cs
@@ -62,11 +62,11 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseCors();
 
