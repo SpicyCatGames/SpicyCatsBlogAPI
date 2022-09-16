7223aec32ac391c5a898f09628208b46d51175ad
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-09-16 11:14:14 +0600

    Create PostDto.cs

---
+++ SpicyCatsBlogAPI/Models/Content/PostDto.cs
@@ -0,0 +1,17 @@
+﻿namespace SpicyCatsBlogAPI.Models.Content
+{
+    public class PostDto
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; } = "";
+        public string Body { get; set; } = "";
+        public string Image { get; set; } = "";
+
+        public string Description { get; set; } = "";
+        public string Tags { get; set; } = "";
+        public string Category { get; set; } = "";
+
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
