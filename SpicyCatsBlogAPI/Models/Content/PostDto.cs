e2dea156dd748b6b675606df94428ee7c303b5f7
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-09-17 16:11:50 +0600

    add property ImageUrl to PostDto

--- SpicyCatsBlogAPI/Models/Content/PostDto.cs
+++ SpicyCatsBlogAPI/Models/Content/PostDto.cs
@@ -6,7 +6,8 @@
 
         public string Title { get; set; } = "";
         public string Body { get; set; } = "";
-        public IFormFile Image { get; set; }
+        public IFormFile Image { get; set; } = null;
+        public string ImageUrl { get; set; } = "";
 
         public string Description { get; set; } = "";
         public string Tags { get; set; } = "";
