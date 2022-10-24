c638248679e60553aacc447bcdf5bd7e7205ab6b
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-10-24 10:48:14 +0600

    Simplified Validation Error response
    {
      "errors": [
        "Username: Username is missing",
        "Username: Username must be at least 3 characters long"
      ]
    }

--- SpicyCatsBlogAPI/Utils/ActionFilters/Validation/ValidationFailedResult.cs
+++ SpicyCatsBlogAPI/Utils/ActionFilters/Validation/ValidationFailedResult.cs
@@ -6,7 +6,7 @@ namespace SpicyCatsBlogAPI.Utils.ActionFilters.Validation
     public class ValidationFailedResult : ObjectResult
     {
         public ValidationFailedResult(ModelStateDictionary modelState)
-            : base(new ValidationResultModel(modelState))
+            : base(new CustomErrorResultModel(modelState))
         {
             StatusCode = StatusCodes.Status422UnprocessableEntity; //change the http status code to 422.
         }
