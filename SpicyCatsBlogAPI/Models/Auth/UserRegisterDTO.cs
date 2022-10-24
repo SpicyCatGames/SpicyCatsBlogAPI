64d0a36a9a97c2f8511e36d84adfce3f3361ed66
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-10-24 10:13:49 +0600

    Update UserRegisterDTO.cs

--- SpicyCatsBlogAPI/Models/Auth/UserRegisterDTO.cs
+++ SpicyCatsBlogAPI/Models/Auth/UserRegisterDTO.cs
@@ -4,10 +4,10 @@ namespace SpicyCatsBlogAPI.Models.Auth
 {
     public class UserRegisterDto
     {
-        [Required]
+        [Required(ErrorMessage = "Username is missing")]
         [MinLength(3, ErrorMessage = "Username must be at least 3 characters long")]
         public string Username { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Password is missing")]
         [MinLength(4, ErrorMessage = "Password must be at least 4 characters long")]
         public string Password { get; set; }
     }
