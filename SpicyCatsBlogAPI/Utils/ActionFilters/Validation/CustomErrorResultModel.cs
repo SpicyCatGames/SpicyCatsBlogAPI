32b32389808fc68db3d231cde72c760757af7fb3
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-10-24 10:48:14 +0600

    Simplified Validation Error response
    {
      "errors": [
        "Username: Username is missing",
        "Username: Username must be at least 3 characters long"
      ]
    }

---
+++ SpicyCatsBlogAPI/Utils/ActionFilters/Validation/CustomErrorResultModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json;
+
+// https://learn.microsoft.com/en-us/answers/questions/620570/net-core-web-api-model-validation-error-response-t.html
+
+namespace SpicyCatsBlogAPI.Utils.ActionFilters.Validation
+{
+    public class CustomErrorResultModel
+    {
+        public List<String> Errors { get; }
+        public CustomErrorResultModel(ModelStateDictionary modelState)
+        {
+            Errors = modelState.Keys
+                    .SelectMany(key => modelState[key].Errors.Select(x => $"{key}: {x.ErrorMessage}"))
+                    .ToList();
+        }
+    }
+}
