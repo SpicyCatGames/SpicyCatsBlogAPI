e24287dabafd969a999d29f8a244e9df97b8da17
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-10-26 12:22:12 +0600

    Create IErrorResponse, IErrorResponseModel

---
+++ SpicyCatsBlogAPI/Models/Error/IErrorResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpicyCatsBlogAPI.Models.Error
+{
+    public interface IErrorResponse<T> where T : IErrorResponseModel
+    {
+        public List<T> Errors { get; }
+    }
+    public interface IErrorResponseModel
+    {
+        public string Message { get; set; }
+    }
+}
--- SpicyCatsBlogAPI/Utils/ActionFilters/Validation/ValidationErrorResponse.cs
+++ SpicyCatsBlogAPI/Utils/ActionFilters/Validation/ValidationErrorResponse.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Newtonsoft.Json;
+using SpicyCatsBlogAPI.Models.Error;
 
 // https://learn.microsoft.com/en-us/answers/questions/620570/net-core-web-api-model-validation-error-response-t.html
 
 namespace SpicyCatsBlogAPI.Utils.ActionFilters.Validation
 {
-    public class ValidationErrorResponse
+    public class ValidationErrorResponse : IErrorResponse<ValidationErrorModel>
     {
         public List<ValidationErrorModel> Errors { get; } = new List<ValidationErrorModel>();
+
         public ValidationErrorResponse(ModelStateDictionary modelState)
         {
             Errors = modelState.Keys
@@ -20,7 +22,7 @@ namespace SpicyCatsBlogAPI.Utils.ActionFilters.Validation
         }
     }
 
-    public class ValidationErrorModel
+    public class ValidationErrorModel : IErrorResponseModel
     {
         public string Field { get; set; }
         public string Message { get; set; }
